344b22c56b1f0b93107da8fb02af596f857df410
codernative <codernative@gmail.com>
2020-07-14 06:56:46 +0300

    Updates

---
+++ precache-manifest.94848a24588cca686ed4cdca056a46c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d12c5da601f60520683ac31c09ac3f",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "42fa940e44bfdb51d82e",
+    "url": "/react-gh-pages/static/css/main.e6db2736.chunk.css"
+  },
+  {
+    "revision": "83e77ecf945ab4399c3a",
+    "url": "/react-gh-pages/static/js/2.3e4efc95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.3e4efc95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42fa940e44bfdb51d82e",
+    "url": "/react-gh-pages/static/js/main.8c191103.chunk.js"
+  },
+  {
+    "revision": "82ee08065bc1c8797425",
+    "url": "/react-gh-pages/static/js/runtime-main.cc007658.js"
+  }
+]);
